9939ee71b25571ec5c46a36850c1ccc04aab4668
Almir Rogério Camolesi <camolesi@fema.edu.br>
2023-04-18 22:45:55 -0300

    Atualização Conexão.php
    Atualização com classe de conexão

---
+++ conexao.php
@@ -0,0 +1,36 @@
+<?php
+
+class Conexao{
+    private static $dbNome='servicos'; 
+    private static $dbHost = 'localhost';
+    private static $dbUsuario = 'root'; 
+    private static $dbSenha = '';  
+
+    private static $cont = null; 
+
+    public function __construct()
+    {
+        die("A função init não é permitida"); 
+    }
+
+    public static function conectar(){
+        if (self::$cont == null){
+            try{
+               // self::$cont = new PDO("mysql:host=localhost;dbname=servicos", "root", "senha");
+               self::$cont = new PDO("mysql:host=". self::$dbHost .";dbname= " . self::$dbNome , self::$dbUsuario, self::$dbSenha);
+            }
+            catch (PDOException $exception) {
+                die ($exception->getMessage());
+            }
+ 
+        }
+        return self::$cont; 
+    }
+
+    public static function desconectar (){
+        self::$cont = null; 
+    }
+
+}
+
+?>
