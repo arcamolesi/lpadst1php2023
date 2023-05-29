feb23e72c0e22e48d68f33af62a2864fa66e0ad8
Almir Rogério Camolesi <camolesi@fema.edu.br>
2023-05-29 18:21:58 -0300

    Atualiza login
    Atualiza login

---
+++ menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <title>Menu Principal de Operadores</title>
+</head>
+
+<body>
+    <!-- menu principal superior -->
+
+    <nav>
+        <div class="nav-wrapper blue dark-3">
+            <a href="#" class="brand-logo right">
+                <img src="/lpbcct1php2023/VIEW/imagens/logocarro.jpg" alt="" width="80" height="100">
+            </a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="/lpbcct1php2023/VIEW/operador/lstoperador2.php">Operador</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+        </div>
+    </nav>
+
+
+</body>
+
+</html>
